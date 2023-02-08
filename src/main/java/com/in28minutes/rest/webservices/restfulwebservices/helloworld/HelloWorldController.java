c986c113ba5352c9cbad839bf0f42f49e0d6c3ee
chanchishing <chishing@netvigator.com>
2023-02-08 17:45:40 +0800

    picking up message of different language based of request header parameter Accept-Language

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("/hello-world")
     public String helloWorld(){
@@ -22,6 +31,15 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
+
+    @GetMapping("/hello-world-i18n")
+    public String helloWorldi18n(){
+        Locale locale= LocaleContextHolder.getLocale();
+
+        return messageSource.getMessage("good.morning.message",null,"Default Message",locale);
+        //return "Hello World v2";
+    }
+
 
 
 }
